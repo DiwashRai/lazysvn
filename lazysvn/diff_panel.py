ca5b0441a9918b6ceff5d75ccbc13c4d73838b38
DiwashRai <diwash.rai1993@gmail.com>
2023-12-28 13:04:07 +0000

    Added color coding to diff output

--- lazysvn/diff_panel.py
+++ lazysvn/diff_panel.py
@@ -3,13 +3,25 @@ from textual.containers import VerticalScroll
 from textual.app import ComposeResult
 from textual.widget import Widget
 from textual.reactive import reactive
+from rich.text import Text
+from rich.console import RenderableType
 
 
 class DiffText(Widget):
     output = reactive("")
 
-    def render(self) -> str:
-        return self.output
+    def render(self) -> RenderableType:
+        text = Text()
+        for line in self.output.splitlines():
+            if line.startswith("+"):
+                text.append(line + "\n", style="#8ec07c")
+            elif line.startswith("-"):
+                text.append(line + "\n", style="#eb6f92")
+            elif line.startswith("@"):
+                text.append(line + "\n", style="#56949f")
+            else:
+                text.append(line + "\n")
+        return text
 
 
 class DiffPanel(VerticalScroll):
